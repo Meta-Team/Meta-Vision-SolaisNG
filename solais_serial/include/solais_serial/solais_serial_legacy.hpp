1b6e3dbb65929704fda2decb55ec68dc72c70c3a
nice-mee <chiming2@illinois.edu>
2023-07-26 20:01:18 +0800

    [`solais_serial`] Implement projectile prediction

--- solais_serial/include/solais_serial/solais_serial_legacy.hpp
+++ solais_serial/include/solais_serial/solais_serial_legacy.hpp
@@ -7,21 +7,31 @@
 #include "rclcpp/rclcpp.hpp"
 #include "serial_driver/serial_driver.hpp"
 #include "serial_driver/serial_port.hpp"
-#include "solais_interfaces/msg/armors.hpp"
+#include "auto_aim_interfaces/msg/target.hpp"
+#include "rmoss_projectile_motion/gimbal_transform_tool.hpp"
+
+#include <tf2_ros/transform_broadcaster.h>
+#include <visualization_msgs/msg/marker.hpp>
+
+#include <Eigen/Dense>
+
+// #include "tf2_ros/buffer.h"
 
 namespace solais_serial
 {
 class SerialNodeLegacy
 {
 public:
-  SerialNodeLegacy(const rclcpp::NodeOptions & options);
+  explicit SerialNodeLegacy(const rclcpp::NodeOptions & options);
   ~SerialNodeLegacy();
   rclcpp::node_interfaces::NodeBaseInterface::SharedPtr get_node_base_interface() const;
 
 private:
   void declareParameters();
 
-  void armorCallback(const solais_interfaces::msg::Armors::SharedPtr msg);
+  void receivePackage();
+
+  void sendPackage(const auto_aim_interfaces::msg::Target::SharedPtr msg);
 
   rclcpp::Node::SharedPtr node_;
 
@@ -30,39 +40,46 @@ private:
   long baud_rate_;
   std::shared_ptr<drivers::serial_driver::SerialPortConfig> device_config_;
   std::shared_ptr<drivers::serial_driver::SerialDriver> serial_driver_;
-  rclcpp::Subscription<solais_interfaces::msg::Armors>::SharedPtr armors_sub_;
+  rclcpp::Subscription<auto_aim_interfaces::msg::Target>::SharedPtr armors_sub_;
+  double timestamp_offset_ = 0;
+  std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+
+  // Debug
+  rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_pub_;
+  visualization_msgs::msg::Marker aiming_point_;
+
+
+  // For projectile prediction
+  float cur_pitch_ = 0.;
+  float cur_yaw_ = 0.;
+  double offset_x_;
+  double offset_y_;
+  double offset_z_;
+  double offset_pitch_;
+  double offset_yaw_;
+  double offset_time_;
+  double shoot_speed_;
+  double friction_{0.001};
+
+  std::string solver_type_;
+  std::shared_ptr<rmoss_projectile_motion::ProjectileSolverInterface> solver_;
 };
 
-struct __attribute__((packed, aligned(1))) VisionCommand
+
+struct __attribute__((packed, aligned(1))) SentPackage
 {
-
-  enum VisionFlag : uint8_t
-  {
-    NONE = 0,
-    DETECTED = 1,
-    TOP_KILLER_TRIGGERED = 2,
-  };
-
-  uint8_t flag;
-  uint16_t frameTime;               // [0.1ms]
-  int16_t yawDelta;                 // yaw relative angle [deg] * 100
-  int16_t pitchDelta;               // pitch relative angle [deg] * 100
-  int16_t distance;                 // [mm]
-  int16_t avgLightAngle;            // [deg] * 100
-  int16_t imageX;                   // pixel
-  int16_t imageY;                   // pixel
-  int16_t remainingTimeToTarget;    // [ms]
-  int16_t period;                   // [ms]
-};
-
-struct __attribute__((packed, aligned(1))) Package
-{
-  uint8_t sof;
-  uint8_t cmdID;
-  union {
-    VisionCommand command;
-  };
+  uint8_t sof = 0x5A;  // Start of frame
+  float yaw;
+  float pitch;
   uint8_t crc8;
+};
+
+struct __attribute__((packed, aligned(1))) ReceivedPackage
+{
+  uint8_t sof = 0x5A;  // Start of frame
+  float yaw;
+  float pitch;
+  uint16_t crc16;
 };
 
 enum class CommandID : uint8_t
@@ -71,12 +88,19 @@ enum class CommandID : uint8_t
   CMD_ID_COUNT
 };
 
-std::vector<uint8_t> toVector(const Package & data)
+std::vector<uint8_t> Pak2Vector(const SentPackage & data)
 {
-  std::vector<uint8_t> package(sizeof(Package));
+  std::vector<uint8_t> package(sizeof(SentPackage));
   std::copy(
     reinterpret_cast<const uint8_t *>(&data),
-    reinterpret_cast<const uint8_t *>(&data) + sizeof(Package), package.begin());
+    reinterpret_cast<const uint8_t *>(&data) + sizeof(SentPackage), package.begin());
+  return package;
+}
+
+ReceivedPackage Vector2Pak(const std::vector<uint8_t> & data)
+{
+  ReceivedPackage package;
+  std::copy(data.begin(), data.end(), reinterpret_cast<uint8_t *>(&package));
   return package;
 }
 
--- solais_serial/src/solais_serial_legacy.cpp
+++ solais_serial/src/solais_serial_legacy.cpp
@@ -1,9 +1,13 @@
 #include "solais_serial/solais_serial_legacy.hpp"
 #include <bits/stdint-uintn.h>
+#include <rclcpp/logging.hpp>
 #include <vector>
 #include "rclcpp/qos.hpp"
 #include "serial_driver/serial_port.hpp"
 #include "solais_serial/crc.h"
+#include <limits>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tf2/LinearMath/Quaternion.h>
 
 namespace solais_serial
 {
@@ -15,15 +19,32 @@ SerialNodeLegacy::SerialNodeLegacy(const rclcpp::NodeOptions & options)
 
   declareParameters();
 
+  timestamp_offset_ = node_->declare_parameter("timestamp_offset", 0.0);
+  tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*node_);
+
+  marker_pub_ = node_->create_publisher<visualization_msgs::msg::Marker>("/aiming_point", 10);
+
   //  Open serial port
   serial_driver_->init_port(device_name_, *device_config_);
   if (!serial_driver_->port()->is_open()) {
     serial_driver_->port()->open();
   }
 
-  armors_sub_ = node_->create_subscription<solais_interfaces::msg::Armors>(
-    "/detector/armors", rclcpp::SensorDataQoS(), [this](const solais_interfaces::msg::Armors::SharedPtr msg) {
-      armorCallback(msg);
+  aiming_point_.header.frame_id = "odom";
+  aiming_point_.ns = "aiming_point";
+  aiming_point_.type = visualization_msgs::msg::Marker::SPHERE;
+  aiming_point_.action = visualization_msgs::msg::Marker::ADD;
+  aiming_point_.scale.x = aiming_point_.scale.y = aiming_point_.scale.z = 0.12;
+  aiming_point_.color.r = 1.0;
+  aiming_point_.color.g = 1.0;
+  aiming_point_.color.b = 1.0;
+  aiming_point_.color.a = 1.0;
+  aiming_point_.lifetime = rclcpp::Duration::from_seconds(0.1);
+
+
+  armors_sub_ = node_->create_subscription<auto_aim_interfaces::msg::Target>(
+    "/detector/armors", rclcpp::SensorDataQoS(), [this](const auto_aim_interfaces::msg::Target::SharedPtr msg) {
+      sendPackage(msg);
     });
 }
 
@@ -46,6 +67,15 @@ const
 
 void SerialNodeLegacy::declareParameters()
 {
+  offset_x_ = node_->declare_parameter("projectile.offset_x", 0.0);
+  offset_y_ = node_->declare_parameter("projectile.offset_y", 0.0);
+  offset_z_ = node_->declare_parameter("projectile.offset_z", 0.0);
+  offset_pitch_ = node_->declare_parameter("projectile.offset_pitch", 0.0);
+  offset_yaw_ = node_->declare_parameter("projectile.offset_yaw", 0.0);
+  offset_time_ = node_->declare_parameter("projectile.offset_time", 0.0);
+  shoot_speed_ = node_->declare_parameter("projectile.initial_speed", 15.0);
+  solver_type_ = node_->declare_parameter("projectile.solver_type", "gravity");
+
   using FlowControl = drivers::serial_driver::FlowControl;
   using Parity = drivers::serial_driver::Parity;
   using StopBits = drivers::serial_driver::StopBits;
@@ -86,27 +116,138 @@ void SerialNodeLegacy::declareParameters()
   device_config_ = std::make_shared<drivers::serial_driver::SerialPortConfig>( baud_rate_, flow_control, parity, stop_bits);
 }
 
-void SerialNodeLegacy::armorCallback(const solais_interfaces::msg::Armors::SharedPtr msg)
+void SerialNodeLegacy::receivePackage()
 {
+  std::vector<uint8_t> header(1);
+  std::vector<uint8_t> data;
+  data.reserve(sizeof(ReceivedPackage));
+
+  while (rclcpp::ok()) {
+    try {
+      serial_driver_->port()->receive(header);
+
+      if (header[0] == 0x5A) {
+        data.resize(sizeof(ReceivedPackage) - 1);
+        serial_driver_->port()->receive(data);
+
+        data.insert(data.begin(), header[0]);
+        ReceivedPackage package = Vector2Pak(data);
+
+        bool crc16_check = verifyCRC16CheckSum(reinterpret_cast<uint8_t *>(&package), sizeof(ReceivedPackage));
+
+        if (crc16_check) {
+          RCLCPP_INFO(node_->get_logger(), "Yaw: %f, Pitch: %f", package.yaw, package.pitch);
+        } else {
+          RCLCPP_ERROR(node_->get_logger(), "CRC16 check failed");
+        }
+
+        cur_pitch_ = package.pitch;
+        cur_yaw_ = package.yaw;
+
+        geometry_msgs::msg::TransformStamped t;
+          timestamp_offset_ = node_->get_parameter("timestamp_offset").as_double();
+          t.header.stamp = node_->now() + rclcpp::Duration::from_seconds(timestamp_offset_);
+          t.header.frame_id = "odom";
+          t.child_frame_id = "gimbal_link";
+          tf2::Quaternion q;
+          q.setRPY(0., package.pitch, package.yaw);
+          t.transform.rotation = tf2::toMsg(q);
+          tf_broadcaster_->sendTransform(t);
+      } else {
+        RCLCPP_WARN_THROTTLE(node_->get_logger(), *(node_->get_clock()), 20, "Invalid header: %02X", header[0]);
+      }
+    } catch (const std::exception & e) {
+      RCLCPP_ERROR(node_->get_logger(), "Error receiving data: %s", e.what());
+    }
+  }
+}
+
+void SerialNodeLegacy::sendPackage(const auto_aim_interfaces::msg::Target::SharedPtr msg)
+{
+  if (!msg->tracking) {
+    return;
+  }
+
+  rclcpp::Time target_time = msg->header.stamp;
+  auto center_position =
+    Eigen::Vector3d(
+    msg->position.x + offset_x_, msg->position.y + offset_y_,
+    msg->position.z + offset_z_);
+  auto center_velocity = Eigen::Vector3d(msg->velocity.x, msg->velocity.y, msg->velocity.z);
+
+  // Calculate each target position at current time & predict time.
+  double min_yaw = std::numeric_limits<double>::max();
+  double min_dis = std::numeric_limits<double>::max();
+  double hit_yaw = cur_yaw_, hit_pitch = cur_pitch_;
+  bool is_current_pair = true;
+  double r = 0., target_dz = 0., fly_time = 0.;
+  double target_pitch, target_yaw;
+  Eigen::Vector3d target_position, target_predict_position;
+  double final_x = 0., final_y = 0., final_z = 0.;
+
+  if (msg->armors_num <= 0)
+   return;
+
+  for (int i = 0; i < msg->armors_num; ++i) {
+    double tmp_yaw = msg->yaw + i * (2 * M_PI / msg->armors_num);
+    if (msg->armors_num == 4) {
+      r = is_current_pair ? msg->radius_1 : msg->radius_2;
+      is_current_pair = !is_current_pair;
+      target_dz = is_current_pair ? 0. : msg->dz;
+    } else {
+      r = msg->radius_1;
+      target_dz = 0.;
+    }
+    target_position = center_position + Eigen::Vector3d(
+      -r * std::cos(tmp_yaw), -r * std::sin(tmp_yaw),
+      target_dz);
+
+    // Use distance to calculate the time offset. (Approximate)
+    fly_time = target_position.head(2).norm() / shoot_speed_ + offset_time_;
+    tmp_yaw = tmp_yaw + msg->v_yaw * fly_time;
+    target_predict_position = center_position + center_velocity * fly_time +
+      Eigen::Vector3d(
+      -r * std::cos(tmp_yaw), -r * std::sin(tmp_yaw),
+      target_dz);
+
+    solver_->solve(
+      target_predict_position.head(2).norm(), target_predict_position.z(),
+      target_pitch);
+    target_pitch = -target_pitch;  // Right-handed system
+    target_yaw = std::atan2(target_predict_position.y(), target_predict_position.x());
+
+    // Choose the target with minimum yaw error.
+    if (::abs(
+        ::fmod(
+          tmp_yaw,
+          M_PI) - cur_yaw_) < min_yaw && target_predict_position.head(2).norm() < min_dis)
+    {
+      min_yaw = ::abs(::fmod(tmp_yaw, M_PI) - cur_yaw_);
+      min_dis = target_predict_position.head(2).norm();
+      hit_yaw = target_yaw;
+      hit_pitch = target_pitch;
+      final_x = target_predict_position.x();
+      final_y = target_predict_position.y();
+      final_z = target_predict_position.z();
+    }
+  }
+
+  // Publish debug marker
+  aiming_point_.header.stamp = node_->now();
+  aiming_point_.pose.position.x = final_x;
+  aiming_point_.pose.position.y = final_y;
+  aiming_point_.pose.position.z = final_z;
+  marker_pub_->publish(aiming_point_);
+
   try {
-    Package package;
+    SentPackage package;
 
     package.sof = 0xA5;
-    package.cmdID = static_cast<uint8_t>(CommandID::VISION_CONTROL_CMD_ID);
-    package.command.flag = 0;
-    package.command.flag |= VisionCommand::DETECTED;
-    package.command.frameTime = msg->header.stamp.nanosec / 100000;
-    package.command.yawDelta = 0;
-    package.command.pitchDelta = 0;
-    package.command.distance = 0;
-    package.command.avgLightAngle = 0;
-    package.command.imageX = 0;
-    package.command.imageY = 0;
-    package.command.remainingTimeToTarget = 0;
-    package.command.period = 0;
-    appendCRC8CheckSum((uint8_t *) (&package), sizeof(uint8_t) * 2 + sizeof(VisionCommand) + sizeof(uint8_t));
+    package.pitch = hit_pitch + offset_pitch_;
+    package.yaw = hit_yaw + offset_yaw_;
+    appendCRC8CheckSum((uint8_t *) (&package), sizeof(SentPackage));
 
-    std::vector<uint8_t> package_vec = toVector(package);
+    std::vector<uint8_t> package_vec = Pak2Vector(package);
     serial_driver_->port()->send(package_vec);
 
     auto latency = (node_->now() - msg->header.stamp).seconds() * 1000.0;
